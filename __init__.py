e0834796a09916350b4b72e8338705a1f3ff9a60
Daksh Maahor <daksh.dav2007@gmail.com>
2024-08-19 22:58:01 +0530

    added sql support

---
+++ __init__.py
@@ -0,0 +1,14 @@
+import mysql.connector as sql
+
+connect = sql.connect(host="localhost", user="root", passwd="meradav@2007")
+
+if connect.is_connected():
+    print("connected")
+
+cursor = connect.cursor()
+
+cursor.execute("create database if not exists programming_quiz")
+cursor.execute("use programming_quiz")
+
+cursor.execute("drop table if exists programming_quiz_admin_login")
+cursor.execute("create table programming_quiz_admin_login(user_id varchar(30) not null, user_password varchar(20) not null)")
--- teacher_module.py
+++ teacher_module.py
@@ -2,7 +2,28 @@ import json
 import mysql
 import mysql.connector
 
-print("----   Programming Quiz   ----")
-print("----   Teacher's Module   ----")
+def login():
+    pass
 
+def sign_up():
+    pass
 
+def main():
+    print("----   Programming Quiz   ----")
+    print("----   Teacher's Module   ----")
+
+    print()
+
+    print("Select An Option")
+    print("1. Login")
+    print("2. Sign Up")
+
+    choice = input(">> ")
+
+    if choice == 1:
+        login()
+    elif choice == 2:
+        sign_up()
+    else:
+        print("Invalid Choice")
+        return
